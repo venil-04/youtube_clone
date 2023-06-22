269f9f0a982001f47803dad3de3d1a618e1c2b04
venil-04 <venilsavaliya999@gmail.com>
2023-06-22 14:06:12 +0530

    change to tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-6xl'>hello</div>
+    </>
+  )
+}
+
+export default App
